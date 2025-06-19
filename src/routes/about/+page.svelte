941085a6ab940f8b387a3ae302e21d38268f0af8
marcelohasilva <marcelo.hasilva06@gmail.com>
2025-06-19 17:29:09 -0300

    pagina sobre

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,2 +1,25 @@
-<h1>About</h1>
-<a href="/">Home</a>
+<img class="logoIFPE" src="/logoIFPE.png" alt="Logo do site" />
+
+<h1 class="dev">Desenvolvedores</h1>
+<div class="dev-info">
+    <div class="devs">
+        <div class="dev-info">
+            <img class="dev-img" src="/pica-pau-3.jpg" alt="Desenvolvedor 1" />
+            <div class="dev-informacao">
+            <p class="dev-name">Marcelo Henrque</p>
+            <p class="email"><a href="">mhas8@discente.ifpe.edu.br</a></p>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="buttons">
+    <a href="/">
+        <button class="btnhome">Home</button>
+    </a>
+</div>
+
+<!-- footer -->
+<footer class="rodape">
+    <p>&copy; 2025 Minesweeper. Todos os direitos reservados.</p>
+</footer>
