3fad163fc23167b7a5a1afdfbbfaa241a34d1aac
Marcelo <marcelo.hasilva06@gmail.com>
2025-06-20 13:59:24 -0300

    responsivo

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,4 +1,4 @@
-<img class="logoIFPE" src="/logoIFPE.png" alt="Logo do site" />
+<img class="logoIFPE" src="/logoIFPE.png" alt="Logo da instituição" />
 
 <h1 class="dev">Desenvolvedores</h1>
 <div class="dev-info">
@@ -6,9 +6,58 @@
         <div class="dev-info">
             <img class="dev-img" src="/pica-pau-3.jpg" alt="Desenvolvedor 1" />
             <div class="dev-informacao">
-            <p class="dev-name">Marcelo Henrque</p>
-            <p class="email"><a href="">mhas8@discente.ifpe.edu.br</a></p>
+            <p class="dev-name">Marcelo Henrique</p>
+            <p class="email"><a href="mailto:mhas8@discente.ifpe.edu.br">mhas8@discente.ifpe.edu.br</a></p>
             </div>
+        
+        </div>
+    </div>
+</div>
+<div class="dev-info">
+    <div class="devs">
+        <div class="dev-info">
+            <img class="dev-img" src="/pica-pau-3.jpg" alt="Desenvolvedor 1" />
+            <div class="dev-informacao">
+            <p class="dev-name">Ricardo</p>
+            <p class="email"><a href="mailto:mhas8@discente.ifpe.edu.br">mhas8@discente.ifpe.edu.br</a></p>
+            </div>
+        
+        </div>
+    </div>
+</div>
+<div class="dev-info">
+    <div class="devs">
+        <div class="dev-info">
+            <img class="dev-img" src="/pica-pau-3.jpg" alt="Desenvolvedor 1" />
+            <div class="dev-informacao">
+            <p class="dev-name">Lopes</p>
+            <p class="email"><a href="mailto:mhas8@discente.ifpe.edu.br">mhas8@discente.ifpe.edu.br</a></p>
+            </div>
+        
+        </div>
+    </div>
+</div>
+<div class="dev-info">
+    <div class="devs">
+        <div class="dev-info">
+            <img class="dev-img" src="/pica-pau-3.jpg" alt="Desenvolvedor 1" />
+            <div class="dev-informacao">
+            <p class="dev-name">Thiago</p>
+            <p class="email"><a href="mailto:mhas8@discente.ifpe.edu.br">mhas8@discente.ifpe.edu.br</a></p>
+            </div>
+        
+        </div>
+    </div>
+</div>
+<div class="dev-info">
+    <div class="devs">
+        <div class="dev-info">
+            <img class="dev-img" src="/pica-pau-3.jpg" alt="Desenvolvedor 1" />
+            <div class="dev-informacao">
+            <p class="dev-name">Gabriel</p>
+            <p class="email"><a href="mailto:mhas8@discente.ifpe.edu.br">mhas8@discente.ifpe.edu.br</a></p>
+            </div>
+        
         </div>
     </div>
 </div>
@@ -19,7 +68,7 @@
     </a>
 </div>
 
-<!-- footer -->
+<!-- rodape -->
 <footer class="rodape">
     <p>&copy; 2025 Minesweeper. Todos os direitos reservados.</p>
 </footer>
