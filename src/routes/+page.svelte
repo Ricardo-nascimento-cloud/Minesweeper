4469a531185278a021f2b5b4fe8396cbfef8bcf3
Marcelo <marcelo.hasilva06@gmail.com>
2025-06-23 13:22:53 -0300

    background e cores do site

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <div class="container">
     <div class="menu">
-        <h1 class="title">Minesweeper</h1>
+       <img src="/logo.webp" alt="Minesweeper Logo" class="logo">
         <div class="buttons">
             <a href="/game">
                 <button class="playButton">Play</button>
