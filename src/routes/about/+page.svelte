504feb61b36013915958883767741cd56f3a2a09
Marcelo <marcelo.hasilva06@gmail.com>
2025-08-07 15:57:40 -0300

    about

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,80 +1,56 @@
 <div class="logo">
-    <img class="logoIFPE" src="/logoIFPE-removebg-preview.png" alt="Logo da instituição" />
-    <img class="logoGame" src="/logo.png" alt="Logo do game" />
+  <img class="logoIFPE" src="/logoIFPE-removebg-preview.png" alt="Logo da instituição" />
+  <img class="logoGame" src="/logo.png" alt="Logo do game" />
 </div>
 
-<h1 class="dev">Developers</h1>
-<div class="dev-info">
-    <div class="devs">
-        <div class="dev-info">
-            <img class="dev-img" src="/pica-pau-3.jpg" alt="Desenvolvedor 1" />
-            <div class="dev-informacao">
-            <p class="dev-name">Marcelo Henrique</p>
-            <p class="email"><a href="mailto:mhas8@discente.ifpe.edu.br">mhas8@discente.ifpe.edu.br</a></p>
-            </div>
-        
-        </div>
+<h1 class="dev">DEVELOPERS</h1>
+
+<!-- Lista de desenvolvedores -->
+<div class="menu">
+  <div class="dev-info">
+    <div class="dev-informacao">
+      <p class="dev-name">Marcelo Henrique</p>
+      <p class="email1"><a href="mailto:mhas8@discente.ifpe.edu.br">mhas8@discente.ifpe.edu.br</a></p>
     </div>
-</div>
-<div class="dev-info">
-    <div class="devs">
-        <div class="dev-info">
-            <img class="dev-img" src="/pica-pau-3.jpg" alt="Desenvolvedor 1" />
-            <div class="dev-informacao">
-            <p class="dev-name">Ricardo João</p>
-            <p class="email"><a href="mailto:rjnf1@discente.ifpe.edu.br">rjnf1@discente.ifpe.edu.br</a></p>
-            </div>
-        
-        </div>
+  </div>
+
+  <div class="dev-info">
+    <div class="dev-informacao">
+      <p class="dev-name">Ricardo João</p>
+      <p class="email"><a href="mailto:rjnf1@discente.ifpe.edu.br">rjnf1@discente.ifpe.edu.br</a></p>
     </div>
-</div>
-<div class="dev-info">
-    <div class="devs">
-        <div class="dev-info">
-            <img class="dev-img" src="/pica-pau-3.jpg" alt="Desenvolvedor 1" />
-            <div class="dev-informacao">
-            <p class="dev-name">Gabriel Lopes</p>
-            <p class="email"><a href="mailto:glo3@discente.ifpe.edu.br">glo3@discente.ifpe.edu.br</a></p>
-            </div>
-        
-        </div>
+  </div>
+
+  <div class="dev-info">
+    <div class="dev-informacao">
+      <p class="dev-name">Gabriel Lopes</p>
+      <p class="email"><a href="mailto:glo3@discente.ifpe.edu.br">glo3@discente.ifpe.edu.br</a></p>
     </div>
-</div>
-<div class="dev-info">
-    <div class="devs">
-        <div class="dev-info">
-            <img class="dev-img" src="/pica-pau-3.jpg" alt="Desenvolvedor 1" />
-            <div class="dev-informacao">
-            <p class="dev-name">Thyago Ruan</p>
-            <p class="email"><a href="mailto:trgl@discente.ifpe.edu.br">trgl@discente.ifpe.edu.br</a></p>
-            </div>
-        
-        </div>
+  </div>
+
+  <div class="dev-info">
+    <div class="dev-informacao">
+      <p class="dev-name">Thyago Ruan</p>
+      <p class="email"><a href="mailto:trgl@discente.ifpe.edu.br">trgl@discente.ifpe.edu.br</a></p>
     </div>
-</div>
-<div class="dev-info">
-    <div class="devs">
-        <div class="dev-info">
-            <img class="dev-img" src="/pica-pau-3.jpg" alt="Desenvolvedor 1" />
-            <div class="dev-informacao">
-            <p class="dev-name">Gabriel Ramos</p>
-            <p class="email"><a href="mailto:grs21@discente.ifpe.edu.br">grs21@discente.ifpe.edu.br</a></p>
-            </div>
-        
-        </div>
+  </div>
+
+  <div class="dev-info">
+    <div class="dev-informacao">
+      <p class="dev-name">Gabriel Ramos</p>
+      <p class="email"><a href="mailto:grs21@discente.ifpe.edu.br">grs21@discente.ifpe.edu.br</a></p>
     </div>
+  </div>
 </div>
 
 <div class="buttons">
-    <a href="/" class="btnhome"> 
-       Home
-    </a>
+  <a href="/" class="btnhome">Home</a>
 </div>
 
-<!-- rodape -->
 <footer class="rodape">
-    <p>&copy; 2025 Minesweeper. Todos os direitos reservados.</p>
+  <p>&copy; 2025 Minesweeper. All rights reserved.</p>
 </footer>
+
 
 <svelte:head>
   <style>
